71a368bd9ea6c9e3f266bd05797d12bd6018c883
George Qi <georgeqi94@gmail.com>
2022-08-10 17:49:16 +0000

    [maskedtensor] adding reductions
    ghstack-source-id: 19cea13ccfa7cdb965e7990227d9975342175033
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82839

--- torch/masked/maskedtensor/core.py
+++ torch/masked/maskedtensor/core.py
@@ -342,6 +342,14 @@ class MaskedTensor(torch.Tensor):
     def __torch_function__(cls, func, types, args=(), kwargs=None):
         kwargs = kwargs or {}
 
+        if func is torch.nn.functional.multi_head_attention_forward:
+            from .functions import multi_head_attention_forward as mha_mt
+            return mha_mt(*args, **kwargs)
+
+        from .reductions import apply_reduction, is_reduction
+        if is_reduction(func):
+            return apply_reduction(func, *args, **kwargs)
+
         if func in [torch.Tensor.where, torch.where]:
             _check_args_kwargs_length(args, kwargs, "__torch_function__, torch.where", len_args=3, len_kwargs=0)
             return MaskedWhere.apply(*args)
@@ -368,10 +376,18 @@ class MaskedTensor(torch.Tensor):
 
     @classmethod
     def __torch_dispatch__(cls, func, types, args, kwargs):
-        func = func.overloadpacket
+        if func is torch.ops.aten.stride:
+            return None
 
         if len(args) <= 0:
             raise ValueError("It is required that len(args) > 0 for __torch_dispatch__")
+
+        func = func.overloadpacket
+
+        from .reductions import apply_reduction, is_reduction
+
+        if is_reduction(func):
+            return apply_reduction(func, *args, **kwargs)
 
         from .passthrough import apply_pass_through_fn, is_pass_through_fn
 
@@ -387,6 +403,11 @@ class MaskedTensor(torch.Tensor):
 
         if is_native_binary(func):
             return apply_native_binary(func, *args, **kwargs)
+
+        from .matmul import apply_native_matmul, is_native_matmul
+
+        if is_native_matmul(func):
+            return apply_native_matmul(func, *args, **kwargs)
 
         if func in [torch.ops.aten.mm, torch.ops.aten.bmm]:
             _check_args_kwargs_length(args, kwargs, f"__torch_dispatch__, {func}", len_args=2, len_kwargs=0)
