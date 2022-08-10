4b6406664eab3af726a0cdebfd0ac15396d36646
Fábio Rocha <frocha@quansight.com>
2022-08-10 07:27:13 -0500

    [primTorch] added ref for nn.functional.glu
    ghstack-source-id: cf5765b5948c8cb9dc68b8a881ad95f75ce39565
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/82214

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -18943,6 +18943,12 @@ python_ref_db = [
         ),
     ),
     PythonRefInfo(
+        "_refs.nn.functional.glu",
+        torch_opinfo_name="nn.functional.glu",
+        supports_nvfuser=False,
+        supports_out=True,
+    ),
+    PythonRefInfo(
         "_refs.nn.functional.leaky_relu",
         torch_opinfo_name="nn.functional.leaky_relu",
     ),
